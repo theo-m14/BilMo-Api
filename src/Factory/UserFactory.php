817a9237c2af4666fb27ceb29225aa201c2f8817
Michel Theo <theo.michel2@outlook.fr>
2024-01-16 20:40:06 +0100

    symfony insight recommendation

--- src/Factory/UserFactory.php
+++ src/Factory/UserFactory.php
@@ -29,21 +29,12 @@ use Zenstruck\Foundry\RepositoryProxy;
  */
 final class UserFactory extends ModelFactory
 {
-    /**
-     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services
-     *
-     * @todo inject services if required
-     */
     public function __construct()
     {
         parent::__construct();
     }
 
-    /**
-     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories
-     *
-     * @todo add your default values here
-     */
+
     protected function getDefaults(): array
     {
         return [
