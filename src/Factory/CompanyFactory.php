ad8bac34a82b6a886675b5e7ba9e587e32d3bf00
Michel Theo <theo.michel2@outlook.fr>
2024-01-30 14:52:42 +0100

    Symfony Insight Recommendation

--- src/Factory/CompanyFactory.php
+++ src/Factory/CompanyFactory.php
@@ -29,21 +29,11 @@ use Zenstruck\Foundry\RepositoryProxy;
  */
 final class CompanyFactory extends ModelFactory
 {
-    /**
-     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services
-     *
-     * @todo inject services if required
-     */
     public function __construct()
     {
         parent::__construct();
     }
 
-    /**
-     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories
-     *
-     * @todo add your default values here
-     */
     protected function getDefaults(): array
     {
         return [
@@ -54,13 +44,10 @@ final class CompanyFactory extends ModelFactory
         ];
     }
 
-    /**
-     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
-     */
+
     protected function initialize(): self
     {
         return $this
-            // ->afterInstantiate(function(Company $company): void {})
         ;
     }
 
